83fd84b0bab71d3749566de76c3689b41416d588
Nate Dooley <nxd7989@rit.edu>
2017-09-14 19:31:31 -0400

    a

--- index.php
+++ index.php
@@ -65,8 +65,7 @@
 				</h2>
 				<img class="circular--square" src="assets/img/nate.jpg" />
 				<div class="hero-text">
-					Rochester Institute of Technology | B.S. Computers and Information Technology '22 | Computer Science House
-				</div>
+					Rochester Institute of Technology | B.S. Computing and Information Technologies '22 
 				<a class="cta" style="margin: 38px auto !important; display: block; width: 60px; text-shadow: 2px 2px 8px black;" href="#contact">
 					Hire Me
 				</a>
