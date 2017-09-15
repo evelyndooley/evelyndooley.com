2f35f8c3ffcd4ca7c3c5ab600123e4735bf65b29
Nate Dooley <nxd7989@rit.edu>
2017-09-15 10:55:40 -0400

    a

--- index.php
+++ index.php
@@ -250,7 +250,7 @@
 			});
 		</script>
 	</body>
-			<link rel="stylesheet" href="assets/css/main.css" type="text/css" />
+		<link rel="stylesheet" href="assets/css/main.css" type="text/css" />
 		<link rel="stylesheet" href="assets/css/hamburger.css" type="text/css" />
 		<link rel="stylesheet" href="assets/css/grid.css" type="text/css" />
 		<link href='https://fonts.googleapis.com/css?family=Montserrat:400,700' rel='stylesheet' type='text/css'>
