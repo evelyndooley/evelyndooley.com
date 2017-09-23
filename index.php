3f64bd074e1773f4d20edcdb7a84369ded6586c5
Nate Dooley <nxd7989@rit.edu>
2017-09-23 18:27:32 -0400

    a

--- index.php
+++ index.php
@@ -224,9 +224,13 @@
 				</div>
 				<div class="column-center">
 					<a href="#" class="social-icon fb">
-					<svg xmlns="http://www.w3.org/2000/svg" height="50" viewBox="0 0 32 32" width="50">
-    					<path d="m12.7 16.7-2 0 0-4.1 2 0 0-4.3c0.1-1.9 1.6-3.2 3.6-3.2l4.6 0 0 4.1-2.7 0c-0.5 0-0.7 0.2-0.7 0.8l0 2.6 3.4 0-0.7 4.1-2.7 0 0 10.2-4.8 0z" style="fill:#C3C3C3;stroke-width:0.01"/>
-					</svg>
+					<svg height="50" style="shape-rendering:geometricPrecision; text-rendering:geometricPrecision; image-rendering:optimizeQuality; fill-rule:evenodd; clip-rule:evenodd" viewBox="0 0 2048 2048" width="50" xml:space="preserve" xmlns="http://www.w3.org/2000/svg" xmlns:xlink="http://www.w3.org/1999/xlink"><defs><style type="text/css">
+   					<![CDATA[
+    				.fil2 {fill:none}
+  					.fil0 {fill:#424242}
+  					.fil1 {fill:#424242;fill-rule:nonzero}
+  					]]>
+ 					</style></defs><g id="Layer_x0020_1"><g id="_333160488"><g><path class="fil0" d="M1647.03 1559.85l43.767 0c55.6619,0 101.202,-45.5398 101.202,-101.202l0 -869.293c0,-55.6619 -45.5398,-101.202 -101.202,-101.202l-43.767 0 0 1071.7z" id="_333160872"/><path class="fil0" d="M1028.28 1165.29l-1.1941 0.839764 -3.08977 -2.17559 -3.08977 2.17559 -1.1941 -0.839764c-256.16,-180.406 -507.358,-373.438 -763.717,-562.128l68.3245 -107.101 74.6469 -6.10394 1.99725 -1.80236 620.021 462.933 3.01063 -2.2311 3.01063 2.2311 620.021 -462.933 1.99725 1.80236 74.6469 6.10394 68.3245 107.101c-256.358,188.691 -507.557,381.722 -763.717,562.128z" id="_333160368"/><path class="fil0" d="M400.968 1559.85l-43.767 0c-55.6619,0 -101.202,-45.5398 -101.202,-101.202l0 -869.293c0,-55.6619 45.5398,-101.202 101.202,-101.202l43.767 0 0 1071.7z" id="_333160728"/></g><polygon class="fil1" id="_333160440" points="366.182,1509.25 1681.82,1509.25 1681.82,1559.85 366.182,1559.85 "/><polygon class="fil1" id="_333160752" points="366.182,488.152 1681.82,488.152 1681.82,538.754 366.182,538.754 "/></g></g><rect class="fil2" height="2048" width="2048"/></svg>
 					</a>
 				</div>
 				<div class="column-right">
